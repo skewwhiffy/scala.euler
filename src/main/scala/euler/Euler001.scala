370b6029863e5d9ea8cfcb36641d9609218f1e3b
Kenny Hung <kennysubs+github@outlook.com>
2020-10-22 14:20:00 +0100

    Fix first failing test

--- src/main/scala/euler/Euler001.scala
+++ src/main/scala/euler/Euler001.scala
@@ -2,6 +2,8 @@ package euler
 
 class Euler001 {
   def sumUpTo(upperLimit: Long): Long = {
-    50
+    (0L to upperLimit - 1)
+      .filter(it => it % 3 == 0 || it % 5 == 0)
+      .foldLeft(0L)(_ + _)
   }
 }
